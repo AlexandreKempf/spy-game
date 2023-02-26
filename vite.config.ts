15bd731cbc5943d4866459cab6e93f52cc5451c8
Aleexo <alexandre@lixo.tech>
2023-02-26 16:02:05 +0100

    workir template :)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import UnoCSS from 'unocss/vite'
+import { extractorSvelte } from '@unocss/core'
+import { presetAttributify, presetUno } from 'unocss'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		UnoCSS({
+			extractors: [extractorSvelte], mode: 'svelte-scoped',
+			presets: [
+				presetAttributify({ /* preset options */ }),
+				presetUno(),
+				// ...custom presets
+			],
+		}), sveltekit(),
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
