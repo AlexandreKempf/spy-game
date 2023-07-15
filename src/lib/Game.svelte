eed4cc3e820b2a2e1c6c7756bf2ba0c00b1b2cb5
Laurent Dardelet <dardelet.l@gmail.com>
2023-07-15 19:29:37 +0200

    Refactor globals

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -31,15 +31,16 @@
 
 	let inputs: [boolean, boolean, boolean, boolean] = [false, false, false, false];
 	let motion: [number, number];
-	let orientation: string = 'down';
+	let playerOrientation: number = 2; // start down
+	let controlOrientation: number = -1; // no control
 	let cycle: number = 0;
-	let animationTable: { [key: string]: number } = {
+	let animationTable: { [key: string | number]: number } = {
 		idle: 0,
 		walk: 1152,
-		right: 0,
-		up: 288,
-		left: 576,
-		down: 864
+		0: 0, // right
+		2: 864, // down
+		4: 576, // left
+		6: 288 // up
 	};
 
 	let position: [number, number];
@@ -54,7 +55,8 @@
 		foreground: p5Type.Image,
 		player: p5Type.Image,
 		position: [number, number],
-		orientation: string,
+		playerOrientation: number,
+		controlOrientation: number,
 		cycle: number
 	) {
 		p5.background(lightOn ? 'white' : 'black');
@@ -71,7 +73,7 @@
 		);
 		let animationScore =
 			animationTable[inputs.includes(true) ? 'walk' : 'idle'] +
-			animationTable[orientation] +
+			animationTable[playerOrientation] +
 			playerWidth * cycle;
 
 		p5.image(
@@ -108,7 +110,11 @@
 				width,
 				height
 			);
-			p5.image(nightVisionCaches[getVisionIndex(motion, orientation)], 0, 0);
+			p5.image(
+				nightVisionCaches[controlOrientation != -1 ? controlOrientation : playerOrientation],
+				0,
+				0
+			);
 			p5.image(
 				playerContours,
 				math.ceil(width / 2) - playerWidth / 2,
@@ -137,13 +143,7 @@
 		return (position[0] + position[1] * map.width) * 4;
 	}
 
-	function getVisionIndex(motion: [number, number], orientation: string): number {
-		if (vectorNorm(motion) == 0) {
-			if (orientation == 'right') return 0;
-			if (orientation == 'down') return 2;
-			if (orientation == 'left') return 4;
-			return 6;
-		}
+	function getVisionIndex(motion: [number, number]): number {
 		let angle = math.asin(normalize(motion)[1]) as number;
 		if (motion[0] < 0) angle = math.pi - angle;
 		return math.round((angle / math.pi) * 4 + 8) % 8;
@@ -293,33 +293,35 @@
 			let oldInputs = inputs;
 			motion = [0, 0];
 			inputs = [false, false, false, false];
-			let defaultOrientation: string = orientation;
+			let defaultPlayerOrientation: number = playerOrientation;
 			let getDefault: boolean = false;
 			if (p5.keyIsDown(p5.LEFT_ARROW)) {
 				motion[0] -= 1;
 				inputs[0] = true;
-				defaultOrientation = 'left';
-				if (!oldInputs[0]) orientation = 'left';
+				defaultPlayerOrientation = 4;
+				if (!oldInputs[0]) playerOrientation = 4;
 			} else if (oldInputs[0]) getDefault = true;
 			if (p5.keyIsDown(p5.RIGHT_ARROW)) {
 				motion[0] += 1;
 				inputs[1] = true;
-				defaultOrientation = 'right';
-				if (!oldInputs[1]) orientation = 'right';
+				defaultPlayerOrientation = 0;
+				if (!oldInputs[1]) playerOrientation = 0;
 			} else if (oldInputs[1]) getDefault = true;
 			if (p5.keyIsDown(p5.UP_ARROW)) {
 				motion[1] -= 1;
 				inputs[2] = true;
-				defaultOrientation = 'up';
-				if (!oldInputs[2]) orientation = 'up';
+				defaultPlayerOrientation = 6;
+				if (!oldInputs[2]) playerOrientation = 6;
 			} else if (oldInputs[2]) getDefault = true;
 			if (p5.keyIsDown(p5.DOWN_ARROW)) {
 				motion[1] += 1;
 				inputs[3] = true;
-				defaultOrientation = 'down';
-				if (!oldInputs[3]) orientation = 'down';
+				defaultPlayerOrientation = 2;
+				if (!oldInputs[3]) playerOrientation = 2;
 			} else if (oldInputs[3]) getDefault = true;
-			if (getDefault) orientation = defaultOrientation;
+			if (getDefault) playerOrientation = defaultPlayerOrientation;
+
+			controlOrientation = vectorNorm(motion) != 0 ? getVisionIndex(motion) : playerOrientation;
 
 			let derivedPositionNorm = vectorNorm(motion);
 			if (derivedPositionNorm != 0) {
@@ -353,7 +355,16 @@
 
 			cycle = (cycle + 1) % 6;
 			// display
-			display(p5, background, foreground, player, position, orientation, cycle);
+			display(
+				p5,
+				background,
+				foreground,
+				player,
+				position,
+				playerOrientation,
+				controlOrientation,
+				cycle
+			);
 		};
 	};
 </script>
